81e6bb1c1459e70262cf86cf2b42d613ecc8c5ac
Rifqi1012 <rifqimh1210@gmail.com>
2024-02-20 11:54:25 +0700

    Update : Change Dashboard on PenyuPanel

--- app/Providers/Filament/PenyuPanelProvider.php
+++ app/Providers/Filament/PenyuPanelProvider.php
@@ -36,9 +36,7 @@ class PenyuPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-            ])
+            ->widgets([])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
